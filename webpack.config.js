f44ba7c5b9c709323e76f0217e6e47fb1c204114
Bartek Dybowski <b .dybowski@gmail.com>
2020-03-18 19:06:03 +0100

    webpack configuration added

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  mode: 'development',
+
+  entry: {
+    client: [
+      '@babel/polyfill',
+      './src/client.js'
+    ]
+  },
+  
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [path.resolve(__dirname, 'src')],
+        query: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+          ],
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendor',
+          test: module => /node_modules/.test(module.resource)
+        },
+      },
+    }
+  },
+
+  devtool: 'cheap-module-source-map'
+};
+
+module.exports = config;
